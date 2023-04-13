3f65bc92be23a0f412874f465af51e0881b5fb0c
Misbahudin <mizzc0d3@gmail.com>
2023-04-13 13:22:35 +0700

    zipping collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -103,4 +103,38 @@ class CollectionTest extends TestCase
             'HR' => collect(['Salman', 'Ferdi'])
         ], $result->all());
     }
+
+    public function testZip()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->zip($collection2);
+
+        $this->assertEquals([
+            collect([1, 4]),
+            collect([2, 5]),
+            collect([3, 6]),
+        ], $collection3->all());
+    }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEqualsCanonicalizing([1, 2, 3, 4, 5, 6], $collection3->all());
+    }
+
+    public function testCombine()
+    {
+        $collection1 = collect(['nama', 'address']);
+        $collection2 = collect(['Mizz', 'Tegal']);
+        $collection3 = $collection1->combine($collection2);
+
+        $this->assertEquals([
+            'nama' => 'Mizz',
+            'address' => 'Tegal'
+        ], $collection3->all());
+    }
 }
